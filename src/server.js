af07afe9f11dcabd77f3be832d8e59cbc8fe6481
Duc Anh Ngo <ducanhepl@gmail.com>
2023-04-03 13:15:11 +0700

    update

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require('dotenv').config();   // giup chay dong process.env
 
 let app = express();
 
 //config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
 
 let port = process.env.PORT || 8088;
 app.listen(port, () => {
